966acf5bb67016d7dfdc08c94387fea0fff67682
Jean-Hadrien Chabran <jh@chabran.fr>
2022-02-05 14:54:45 +0100

    Fix fzf not working anymore

--- modules/code/completion.zsh
+++ modules/code/completion.zsh
@@ -1,1 +1,6 @@
 export fpath=(${ASDF_DIR}/completions $fpath)
+
+# https://github.com/junegunn/fzf
+# Reminder: fzf.zsh has completion related code, so it needs to be loaded after compinit
+# so it must be in this file rather than others to ensure that's the case.
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
--- modules/code/env.zsh
+++ modules/code/env.zsh
@@ -4,9 +4,6 @@ export GIT_GET_PATH=$PROJECTS/src
 # https://sw.kovidgoyal.net/kitty/remote-control/
 export KITTY_LISTEN_ON="unix:/tmp/mykitty"
 
-# https://github.com/junegunn/fzf
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
 # https://github.com/sumneko/lua-language-server
 alias luamake=/Users/tech/code/src/github.com/sumneko/lua-language-server/3rd/luamake/luamake
 
